200668cab296964daae4082eb9d4bf2555a56a49
IkuoShige <shigeikuo@gmail.com>
2022-10-27 12:27:46 +0900

    update test_command

--- test.bash
+++ test.bash
@@ -1,5 +1,17 @@
 #!/bin/bash
+#SPDX-FileCopyrightText: 2022 Ikuo Shige                                                         
+#SPDX-License-Identifier: BSD-3-Clause
 
+ng (){
+     echo NG at Line $1
+     res=1
+}
+
+res=0
+
+### I/O TEST ###
 out=$(seq 5 | ./plus)
-
-[ "${out}" = 14 ]
+[ "${out}" = 15 ] || ng ${LINENO}
+  
+[ "$res" = 0 ] && echo OK
+exit $res
--- yamada.bash
+++ yamada.bash
@@ -1,12 +1,16 @@
 #!/bin/bash
+#SPDX-FileCopyrightText: 2022 Ikuo Shige
+#SPDX-License-Identifier: BSD-3-Clause
 
 ng (){
     echo ${1}行目が違うよ
     ret=1
 }
+
 ret=0
+
 a=山田
 [ "$a" = 上田 ] || ng "$LINENO"
 [ "$a" = 山田 ] || ng "$LINENO"
 
-exit $ret
+exit $res
